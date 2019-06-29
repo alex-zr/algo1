a1be35e18d9b85dcb9ac4d4ae04700d5eda13dcc
Max <m.sharov@spd-ukraine.com>
2019-06-29 22:49:51 +0300

    Implemented insert sort

--- src/sharov/session2/insertsort/InsertSort.java
+++ src/sharov/session2/insertsort/InsertSort.java
@@ -1,25 +1,23 @@
 package src.sharov.session2.insertsort;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class InsertSort {
   public static void main(String[] args) {
-    int[] ar = new int[]{3, 1, 8, 5, 3, 1, 0, -7};
+    int[] ar = new int[]{3, 1, 8, 5, 3, 1, 0, -7, 213, 43, 80, -32, -25, 96, 14};
     System.out.println(Arrays.toString(ar));
-    insertSort(ar, Comparator.reverseOrder());
+    insertSort(ar);
     System.out.println(Arrays.toString(ar));
   }
 
-  public static void insertSort(int[] array, Comparator<Integer> comparator) {
+  public static void insertSort(int[] array) {
     if (array == null || array.length <= 1) {
       return;
     }
-
-    for (int sortedIndex = 0; sortedIndex < array.length; sortedIndex++) {
-      for (int unsortedIndex = sortedIndex + 1; unsortedIndex > 0 && unsortedIndex < array.length; unsortedIndex--) {
-        if (comparator.compare(array[unsortedIndex], array[unsortedIndex - 1]) > 0) {
-          swap(array, unsortedIndex, unsortedIndex - 1);
+    for (int sorted = 0; sorted < array.length; sorted++) {
+      for (int unsorted = sorted + 1; unsorted > 0 && unsorted < array.length; unsorted--) {
+        if (array[unsorted] < array[unsorted - 1]) {
+          swap(array, unsorted, unsorted - 1);
         } else {
           break;
         }
