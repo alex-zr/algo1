58d93c8716e9f83ab40a45b83e3fc570c75de9cf
alex-zr <oleksandr.roshchupkin@gmail.com>
2019-07-02 17:08:22 +0300

    Add VectorList

--- src/roshupkin_nikita/binSearch/BinSearch.java
+++ src/roshupkin_nikita/binSearch/BinSearch.java
@@ -6,9 +6,9 @@ public class BinSearch {
         int[] vector = new int[]{1,3,5,7,9,13,15,16,22,35,44,47};
         int serchValue = 44;
 
-        System.out.println("Индекс вхождения искомого элемента :" + BinSearch(vector,serchValue));
+        System.out.println("Индекс вхождения искомого элемента :" + binSearch(vector,serchValue));
     }
-    public static int BinSearch(int[] vector, int serchValue) {
+    public static int binSearch(int[] vector, int serchValue) {
         int left = 1;
         int right = vector.length -1;
 
---
+++ src/roshupkin_nikita/vectorList/VectorList.java
@@ -0,0 +1,35 @@
+package roshupkin_nikita.vectorList;
+
+public class VectorList {
+    private int[] vector;
+    public static final int SIZE = 16;
+    private int length = 0;
+    public VectorList() {
+        this.vector = new int[SIZE];
+    }
+    public int get(int index) {
+        checkIndex(index);
+        return vector[index];
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException(String.valueOf(index));
+        }
+    }
+    public void add(int element) {
+        ensureCapacity();
+        vector[length ++]= element;
+    }
+    public void remove(int index) {
+        System.arraycopy(vector, index+1, vector, index, length-1);
+
+    }
+
+    private void ensureCapacity() {
+        if (length >= vector.length) {
+            int[] newVector = new int[vector.length + SIZE];
+            System.arraycopy(vector, 0, newVector, 0, vector.length);
+            vector = newVector;
+        }
+    }
+}
