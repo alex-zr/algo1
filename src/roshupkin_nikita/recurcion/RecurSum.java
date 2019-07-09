9f2a9c7126599ad5e6f0f85a33c7f17e0dd898a4
alex-zr <oleksandr.roshchupkin@gmail.com>
2019-07-09 19:09:48 +0300

    Add recurcion

---
+++ src/roshupkin_nikita/recurcion/RecurSum.java
@@ -0,0 +1,24 @@
+package roshupkin_nikita.recurcion;
+
+public class RecurSum {
+    public static void main(String[] args) {
+
+
+        System.out.println(sum(5));
+    }
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+    public static int sum (int value, int counter, int sumVal) {
+        sumVal=sumVal+value;
+        if (value == counter) {
+            return sumVal;
+        }
+        int sumSum=sum(value+1,counter, sumVal);
+        return sumSum;
+    }
+}
